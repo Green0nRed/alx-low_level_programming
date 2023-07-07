62cd02e9de2a58dcaf172c77063c16d12ee770ef
Green0nRed <nabilboussabaa@gmail.com>
2023-07-06 17:05:02 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,34 @@
 #include "main.h"
 #include <stdio.h>
+
+
+
+
+/**
+ *@b: char string
+ **/
+
+
+
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int total, power;
-	int len;
+        	int len;
 
-	if (b == NULL)
+         	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len]; len++)
+	        for (len = 0; b[len]; len++)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
 	}
 
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+          	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
 	{
 		if (b[len] == '1')
 			total += power;
 	}
-
-	return (total);
+		return (total);
 }
