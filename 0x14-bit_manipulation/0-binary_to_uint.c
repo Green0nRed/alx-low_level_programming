fddc0b9ae82b5880e5a3aa31e186615eea2f3a13
Green0nRed <nabilboussabaa@gmail.com>
2023-07-06 17:11:44 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * binary_to_uint - Converts a binary number represented as a string to an unsigned integer.
  *
@@ -8,6 +9,7 @@
  * Return: The converted decimal number as an unsigned int. If the input string is NULL or
  *         contains any character other than '0' and '1', the function returns 0.
  */
+
 unsigned int binary_to_uint(const char *b) {
     unsigned int total = 0;
     unsigned int power = 1;
