079a22c890ec8c96a32873aa47afd0dde4b89d56
Green0nRed <nabilboussabaa@gmail.com>
2023-07-06 17:32:40 -0700

    C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,12 +12,12 @@ unsigned int binary_to_uint(const char *b)
 	int len;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	for (len = 0; b[len]; len++)
 	{
 		if (b[len] != '0' && b[len] != '1')
-			return 0;
+			return (0);
 	}
 
 	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
@@ -26,5 +26,5 @@ unsigned int binary_to_uint(const char *b)
 			total += power;
 	}
 
-	return total;
+	return (total);
 }
