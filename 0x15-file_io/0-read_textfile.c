bdd83a5497ffa3819e335a06de6415bc25e512db
Green0nRed <nabilboussabaa@gmail.com>
2023-09-08 06:35:51 -0700

    0x15. C - File

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,20 @@
 #include "main.h"
-#include <stdlib.h>
-/**
- * read_textfile = print the POSIX stdout with Reading a text file.
- * @filename: the pointers attribuate the name of the file.
- * @letters: the functions will be read and print 
- * considerin the numbers of the letter .
- * The return = if the functions fail or filenames is NULL or 0 .
- * OP/WR - the actual number of bytes the function can read and print.
- */
+#include "stdlib.h"
+/** xX||read_textfile:Reads a Text File And Prints It To POSIX Stdout.||Xx     
+ xX||@filename:A Pointer To The Name Of The File.||Xx
+ xX||@letters:The Number Of Letters The Function Should Read And Print.||Xx                   
+ xX||Return:If The Function Fails Or Filename Is (NULL) - 0. ||Xx
+ xX||O/w:The Actual Number Of Bytes The Function Can Read And Print. ||Xx */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t o, r, w;
-char *buff;
-if (filename == NULL)
+ssize_t o, r, w;char *Buff; if (filename == NULL)
 return (0);
-buff = malloc(sizeof(char) * letters);
-if (buff == NULL)
+Buff = malloc(sizeof(char) * letters); if (Buff == NULL)
 return (0);
-o = open(filename, O_RDONLY);
-r = read(o, buff, letters);
-w = write(STDOUT_FILENO, buff, r);
+o = open(filename, O_RDONLY); r = read(o, Buff, letters); w = write(STDOUT_FILENO, Buff, r);
 if (o == -1 || r == -1 || w == -1 || w != r)
-{
-free(buff);
-return (0);
-}
-free(buff);
-close(o);
+{free(Buff);
+return (0);}
+free(Buff); close(o);
 return (w);
 }
