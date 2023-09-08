999b53f035d8ba04738c5b99ba1bc99f95c9c441
Green0nRed <nabilboussabaa@gmail.com>
2023-09-08 05:04:52 -0700

    0x15. C - File

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * read_textfile = print the POSIX stdout with Reading a text file.
  * @filename = the pointers attribuate the name of the file.
@@ -8,24 +10,27 @@
  * The return = if the functions fail or filenames is NULL or 0 .
  * OP/WR - the actual number of bytes the function can read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t op,re,wr;
-char *buffers;
-if(filename == 0)
-return(NULL);
-buffers = malloc(sizeof(char) * letters);
-if(buffers == 0)
-return(NULL);
-op = open(filename, O_RDONLY);
-re = read(o, buffers, letters);
-wr = write(STDOUT_FILENO, buffers, r);
-if(op == -1 || re == -1 || wr == -1 || wr != r)
-{
-free(buffers);
+char *buffer;
+ssize_t rbytes = 0, wbytes = 0;
+int fdx;
+if (!filename || letters <= 0)
 return (NULL);
-}
-free(buffers);
-close(o);
-return (wr);
+fdx = open(filename, O_RDONLY);
+if (fdx == -1)
+return (NULL);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (NULL);
+rbytes = read(fdx, buffer, letters);
+if (rbytes == -1)
+return (NULL);
+wbytes = write(STDOUT_FILENO, buffer, rbytes);
+if (wbytes == -1 || wbytes < rbytes)
+return (NULL);
+free(buffer);
+close(fdx);
+return (wbytes);
 }
